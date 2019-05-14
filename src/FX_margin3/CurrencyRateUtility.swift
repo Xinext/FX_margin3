fcdf9b30f88e15f20267872b9f5f41e3dbe47261
hiro mbp <guest@guest.com>
2019-05-14 12:12:32 +0900

    [mod] ver.3.3.0 インターネットからの為替レート取得先を変更

--- src/FX_margin3/CurrencyRateUtility.swift
+++ src/FX_margin3/CurrencyRateUtility.swift
@@ -24,18 +24,27 @@ class CurrencyRateUtility: NSObject {
         case NZD = "NZD"
     }
 
+    struct JsonRate: Codable {
+        var JPY: Double?
+        var USD: Double?
+    }
+
+    struct JsonBase: Codable {
+        var base: String
+        var date: String
+        var rates: JsonRate
+    }
+    
     static func GetCrossYenRate(pair: JPY_PAIR) -> Decimal? {
 
-        let pair = pair.rawValue + "_JPY"
-        let url: URL = URL(string: "https://free.currencyconverterapi.com/api/v3/convert?q=" + pair + "&compact=ultra")!
+        let url: URL = URL(string: "https://api.exchangeratesapi.io/latest?base=" + pair.rawValue + "&symbols=JPY")!
         
         guard let price_data = try? Data(contentsOf: url) else { return nil }
 
         var rtn_price: Decimal? = nil
         do {
-            let price_json = try JSONSerialization.jsonObject(with: price_data, options: JSONSerialization.ReadingOptions.allowFragments)
-            let price_dct = price_json as! NSDictionary
-            rtn_price = Decimal(price_dct[pair] as! Double).rounded(3)
+            let price_json = try JSONDecoder().decode(JsonBase.self, from: price_data)
+            rtn_price = Decimal(price_json.rates.JPY!).rounded(3)
         } catch {
             rtn_price = nil
         }
@@ -45,16 +54,14 @@ class CurrencyRateUtility: NSObject {
     
     static func GetCrossDollarRate(pair: USD_PAIR) -> Decimal? {
         
-        let pair = pair.rawValue + "_USD"
-        let url: URL = URL(string: "https://free.currencyconverterapi.com/api/v3/convert?q=" + pair + "&compact=ultra")!
-        
+        let url: URL = URL(string: "https://api.exchangeratesapi.io/latest?base=" + pair.rawValue + "&symbols=USD")!
+
         guard let price_data = try? Data(contentsOf: url) else { return nil }
         
         var rtn_price: Decimal? = nil
         do {
-            let price_json = try JSONSerialization.jsonObject(with: price_data, options: JSONSerialization.ReadingOptions.allowFragments)
-            let price_dct = price_json as! NSDictionary
-            rtn_price = Decimal(price_dct[pair] as! Double).rounded(5)
+            let price_json = try JSONDecoder().decode(JsonBase.self, from: price_data)
+            rtn_price = Decimal(price_json.rates.USD!).rounded(5)
         } catch {
             rtn_price = nil
         }
